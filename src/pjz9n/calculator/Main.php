d53c2f4c6c63b974ee5f1851cbce1dd976996a4d
PJZ9n <pjz9n@yahoo.co.jp>
2020-06-22 09:37:56 +0900

    フォームと計算処理実装

--- src/pjz9n/calculator/Main.php
+++ src/pjz9n/calculator/Main.php
@@ -23,22 +23,140 @@ declare(strict_types=1);
 
 namespace pjz9n\calculator;
 
+use dktapps\pmforms\CustomForm;
+use dktapps\pmforms\CustomFormResponse;
+use dktapps\pmforms\element\Input;
+use dktapps\pmforms\element\Label;
+use dktapps\pmforms\FormIcon;
+use dktapps\pmforms\MenuForm;
+use dktapps\pmforms\MenuOption;
+use pjz9n\resourcepacktools\FileResourcePack;
+use pjz9n\resourcepacktools\ResourcePackVersion;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\form\FormValidationException;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use ReflectionException;
 
 class Main extends PluginBase
 {
+    /**
+     * @throws ReflectionException
+     */
     public function onEnable(): void
     {
         $this->saveDefaultConfig();
+        $pack = new FileResourcePack($this->getDataFolder() . "resource.zip", $this, new ResourcePackVersion(1, 0, 0));
+        $pack->setIcon("images/icon.png");
+        $pack->addFile("images/calculator.png", "form/calculator.png");
+        $pack->addFile("images/settings.png", "form/settings.png");
+        $pack->registerResourcePack();
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
         switch ($command) {
             case "calculator":
-                return false;
+                if (!($sender instanceof Player)) {
+                    $sender->sendMessage(TextFormat::RED . "このコマンドはプレイヤーから実行してください。");
+                    return true;
+                }
+                $sender->sendForm(new MenuForm(
+                    (string)$this->getConfig()->get("title", "Unknown"),
+                    "選択してください！",
+                    [
+                        new MenuOption(
+                            "計算",
+                            new FormIcon(
+                                "calculator/form/calculator",
+                                FormIcon::IMAGE_TYPE_PATH
+                            ),
+                        ),
+                        new MenuOption(
+                            "設定",
+                            new FormIcon(
+                                "calculator/form/settings",
+                                FormIcon::IMAGE_TYPE_PATH
+                            ),
+                        ),
+                    ],
+                    function (Player $player, int $selectedOption): void {
+                        switch ($selectedOption) {
+                            case 0:
+                                //計算
+                                $player->sendForm(new CustomForm(
+                                    (string)$this->getConfig()->get("title", "Unknown"),
+                                    [
+                                        new Input(
+                                            "a",
+                                            "A"
+                                        ),
+                                        new Label(
+                                            "+",
+                                            "+"
+                                        ),
+                                        new Input(
+                                            "b",
+                                            "B"
+                                        ),
+                                    ],
+                                    function (Player $player, CustomFormResponse $customFormResponse): void {
+                                        $a = $customFormResponse->getString("a");
+                                        $b = $customFormResponse->getString("b");
+                                        if (!is_numeric($a) ||
+                                            !is_numeric($b)) {
+                                            $player->sendMessage(TextFormat::RED . "入力された値が不正です。");
+                                            return;
+                                        }
+                                        $a = (int)$a;
+                                        $b = (int)$b;
+                                        if ($a < PHP_INT_MIN ||
+                                            $a > PHP_INT_MAX ||
+                                            $b < PHP_INT_MIN ||
+                                            $b > PHP_INT_MAX) {
+                                            $player->sendMessage(TextFormat::RED . "入力された値の範囲が不正です。");
+                                            return;
+                                        }
+                                        $player->sendMessage(
+                                            TextFormat::AQUA . (string)$a .
+                                            TextFormat::YELLOW . " + " .
+                                            TextFormat::AQUA . (string)$b .
+                                            TextFormat::YELLOW . " = " .
+                                            TextFormat::GREEN . ($a + $b)
+                                        );
+                                    }
+                                ));
+                                break;
+                            case 1:
+                                //設定
+                                $player->sendForm(new CustomForm(
+                                    (string)$this->getConfig()->get("title", "Unknown"),
+                                    [
+                                        new Input(
+                                            "title",
+                                            "タイトル",
+                                            "",
+                                            (string)$this->getConfig()->get("title", "Unknown"),
+                                        ),
+                                    ],
+                                    function (Player $player, CustomFormResponse $customFormResponse): void {
+                                        $this->getConfig()->set("title", $customFormResponse->getString("title"));
+                                        $player->sendMessage(
+                                            TextFormat::AQUA . "タイトルを" .
+                                            $customFormResponse->getString("title") .
+                                            TextFormat::RESET . TextFormat::AQUA . "に設定しました！"
+                                        );
+                                    }
+                                ));
+                                break;
+                            default:
+                                throw new FormValidationException("unexcepted option " . $selectedOption);
+                        }
+                    }
+                ));
+                return true;
         }
         return false;
     }
